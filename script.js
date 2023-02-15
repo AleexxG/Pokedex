6bf51fe84fdadc635c442cb5bafd8b2157b1d6ef
AleexxG <alexanderg.sln@gmail.com>
2023-02-15 23:31:04 +0100

    Type Colors

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
-const pokemonCont = document.querySelector(".pokemon_cont");
+const pokemon_cont = document.querySelector(".pokemon_cont");
 
 const pokemons = [" "];
+const pokemon_limit = 12;
+const type_colors = {
+    'normal': '#BCBCAC',
+    'fighting': '#BC5442',
+    'flying': '#669AFF',
+    'poison': '#AB549A',
+    'ground': '#DEBC54',
+    'rock': '#BCAC66',
+    'bug': '#ABBC1C',
+    'ghost': '#6666BC',
+    'steel': '#ABACBC',
+    'fire': '#FF421C',
+    'water': '#2F9AFF',
+    'grass': '#78CD54',
+    'electric': '#FFCD30',
+    'psychic': '#FF549A',
+    'ice': '#78DEFF',
+    'dragon': '#7866EF',
+    'dark': '#785442',
+    'fairy': '#FFACFF',
+};
 
 const getPokemon = async () => {
-    const endpoint = new URL( "https://pokeapi.co/api/v2/pokemon?limit=12" );
+    const endpoint = new URL( `https://pokeapi.co/api/v2/pokemon?limit=${pokemon_limit}` );
 
     const response = await fetch(endpoint);
     const data = await response.json();
     
-    for ( let i = 0; i < data.results.length; i++ ) {
+    for ( let i = 0; i < pokemon_limit; i++ ) {
         pokemons.push({
             id: i + 1,
             name: data.results[i].name,
             types: []
         });
-        await getAllTypes();
-        displayPokemon(i + 1);
     }
+    await getAllTypes();
+    displayPokemon();
 };
 
-const displayPokemon = (pokemon) => {
-    const name = pokemons[pokemon].name?.charAt(0).toUpperCase() + pokemons[pokemon].name?.slice(1);
-    const id = pokemons[pokemon].id;
-    const type = pokemons[pokemon].types;
-  
-    const pokemonBox = document.createElement("div");
-    pokemonBox.classList.add("pokemon_box");
+const displayPokemon = () => {
+    for ( let i = 1; i < pokemon_limit + 1; i++ ) {
+        const pokemonBox = document.createElement("div");
+        pokemonBox.classList.add("pokemon_box");
 
-    pokemonBox.innerHTML = `
-    <img class="pokemon_img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" alt="Pokemon">
-    <div class="ms-3">
-        <p class="pokemon_num my-1">#${id}</p>
-        <h3>${name}</h3>
-        <div class="type_cont d-flex gap-1">
-            <p class="grass">${type[0]}</p>
-            <p class="poison">${type[1]}</p>
+        pokemonBox.innerHTML = `
+        <img class="pokemon_img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemons[i].id}.png" alt="Pokemon">
+        <div class="ms-3">
+            <p class="pokemon_num my-1">#${pokemons[i].id}</p>
+            <h3>${pokemons[i].name?.charAt(0).toUpperCase() + pokemons[i].name?.slice(1)}</h3>
+            <div class="type_cont d-flex gap-1">
+                <p class="type_color" style="background: ${type_colors[pokemons[i].types[1]]}">${pokemons[i].types[1]}</p>
+                <p class="type_color" style="background: ${type_colors[pokemons[i].types[0]]}">${pokemons[i].types[0]}</p>
+            </div>
         </div>
-    </div>
-    `;
+        `;
   
-    pokemonCont.appendChild(pokemonBox);
+        pokemon_cont.appendChild(pokemonBox);
+    }
 };
 
 const getAllTypes = async () => {
